cab35e1c0ef5e58ca6c83166eb63cb514df2ac3c
Grant <gmwu812@gmail.com>
2024-03-19 17:00:53 -0500

    add delete button functionality

--- src/DOM.js
+++ src/DOM.js
@@ -1,7 +1,7 @@
 import createList from "./list.js"
 
 export default function DOMHandler() {
-    const ul = document.querySelector("#taskList")
+    const taskList = document.querySelector("#taskList")
     const tasks = createList();
     const title = document.querySelector("#title")
     const description = document.querySelector("#description")
@@ -10,21 +10,54 @@ export default function DOMHandler() {
     
 
     const clearList = () => {
-        while(ul.firstChild){
-            ul.removeChild(ul.firstChild);
+        while(taskList.firstChild){
+            taskList.removeChild(taskList.firstChild);
         }
     }
 
     const createListItem = (task) => {
-        let li = document.createElement("li");
-        li.textContent = task
-        ul.appendChild(li);
+        let todo = document.createElement("div");
+        todo.classList.add("todo")
+
+        let taskName = document.createElement("div")
+        taskName.classList.add("taskName");
+        taskName.textContent = task.title
+
+        let buttons = document.createElement("div")
+        buttons.classList.add("buttons")
+
+        const editButton = document.createElement("button")
+        editButton.classList.add("editButton")
+        editButton.textContent = "edit"
+
+        const deleteButton = document.createElement("button")
+        deleteButton.classList.add("deleteButton")
+        deleteButton.textContent = "delete"
+        deleteButton.addEventListener('click', function () {
+            deleteTodo(task)
+        })
+
+        buttons.appendChild(editButton)
+        buttons.appendChild(deleteButton)
+        todo.appendChild(taskName)
+        todo.appendChild(buttons)
+        taskList.appendChild(todo);
+        console.log("added")
+    }
+
+    const deleteTodo = (task) => {
+        // let todo = e.target.parentElement.parentElement
+        // todo.remove()
+        //console.log(tasks.array.indexOf(task))
+        let index = tasks.array.indexOf(task)
+        tasks.removeTask(index)
+        update()
     }
 
     const update = () => {
         clearList();
         tasks.array.forEach(function (task) {
-            createListItem(task.title);
+            createListItem(task);
         })
     }
 
@@ -52,6 +85,7 @@ export default function DOMHandler() {
             update()
         })
     }
+
 
     return {setupModal, clearList, update}
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import createList from './list.js'
 import DOMHandler from './DOM.js'
-
-
+import './styles/style.css'
 
 let DOM = DOMHandler()
 DOM.setupModal()
